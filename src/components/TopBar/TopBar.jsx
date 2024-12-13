f0f1c3a0317b928e93ec45ee6836797ff9108021
Joseph Quesada <josephquesada92@gmail.com>
2024-12-13 17:46:20 -0600

    adding new logo

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,19 +1,21 @@
 import "./TopBar.css";
+import logo from '../../assets/JS.svg';
 
 const TopBar = ({ user, onLogout }) => {
-  
   return (
     <div className="topbar">
       <div className="topbar-content">
-        <div className="logo">DocManager</div>
+        <div className="logo">
+          <img src={logo} alt="DocManager Logo" />
+        </div>
         {user ? (
           <div className="user-info">
             <img
-              src={user.picture}
-              alt={user.name || "User"}
+              src={user?.picture || '/path-to-default-avatar.png'}
+              alt={user?.name || 'User'}
               className="user-avatar"
             />
-            <span>{user.name}</span>
+            <span>{user?.name || 'Guest'}</span>
             <button className="logout-btn" onClick={onLogout}>
               Logout
             </button>
